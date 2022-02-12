dbd10114402df28e06202c9ed75ca64d068ae028
Jasper-KIXX <jasper@digitalkixx.be>
2022-02-12 20:44:41 +0100

    failed test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const images = {
+  images: {
+    //enter the domain or subdomain where you have WordPress installed
+    domains: ['localhost::8888'],
+  },
+}
+
+module.exports = { nextConfig, images }
