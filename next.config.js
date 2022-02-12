02ef661a93eb3d9cc2e673254ab0360e6474adcb
Jasper-KIXX <jasper@digitalkixx.be>
2022-02-12 23:48:37 +0100

    Added images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    //enter the domain or subdomain where you have WordPress installed
+    domains: ['localhost'],
+  }
 }
-
-module.exports = nextConfig
---
+++ utils/wordpress.js
@@ -0,0 +1,67 @@
+const BASE_URL = 'http://localhost:8888/njswp_be/wp-json/wp/v2';
+const MEDIA_URL = 'http://localhost:8888/njswp_be/wp-content/uploads';
+
+export async function getPosts() {
+    const postsRes = await fetch(BASE_URL + '/posts?_embed');
+    const posts = await postsRes.json();
+    return posts;
+}
+
+export async function getPost(slug) {
+    const posts = await getPosts();
+    const postArray = posts.filter((post) => post.slug == slug);
+    const post = postArray.length > 0 ? postArray[0] : null;
+    return post;
+}
+
+export async function getEvents() {
+    const eventsRes = await fetch(BASE_URL + '/events?_embed');
+    const events = await eventsRes.json();
+    return events;
+}
+
+export async function getEvent(slug) {
+    const events = await getEvents();
+    const eventArray = events.filter((event) => event.slug == slug);
+    const event = eventArray.length > 0 ? eventArray[0] : null;
+    return event;
+}
+
+export async function getFeaturedMedia(slug) {
+    const post = await getPost(slug)
+    // console.log(_embedded);
+    // const {
+    //     _embedded: {
+    //         "wp:featuredmedia": {
+    //             media_details: {
+    //                 file
+    //             }
+    //         }
+    //     }
+    // } = post;
+    // console.log(file);
+    // const featuredMedia = `${MEDIA_URL}/${file}`;
+    // const featuredMedia = post['_embedded']['wp:featuredmedia'][0]['media_details']['file'];
+    const featuredMedia = 'http://localhost:8888/njswp_be/event/18/02_majestic-padel_visuals-februari1/';
+    return featuredMedia;
+}
+
+export async function getSlugs(type) {
+    let elements = [];
+    switch (type) {
+        case 'posts':
+            elements = await getPosts();
+            break;
+        case 'events':
+            elements = await getEvents();
+            break;
+    }
+    const elementsIds = elements.map((element) => {
+        return {
+            params: {
+                slug: element.slug,
+            },
+        };
+    });
+    return elementsIds;
+}
